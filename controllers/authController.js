2e87d33a95e667779997689f8a55534ae8756c0d
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2018-12-30 14:58:16 -0800

    implemented password hashing and assignment of jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,20 @@
+const jwt = require("jwt-simple");
 const User = require("../models/user");
+const keys = require("../config/keys");
+
+const tokenForUser = user => {
+  // generates a jwt based on userSecret
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, keys.userSecret);
+}
 
 exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Email and password must be provided." });
+  }
 
   // see if user with given email exists. `User` refers to all users, not just one.
   User.findOne({ email: email }, (err, existingUser) => {
@@ -23,7 +35,7 @@ exports.signup = (req, res, next) => {
     userToSave.save(err => {
       if (err) { return next(err); }
 
-      res.json({ success: true }); // response to request indicating creation of user.
+      res.json({ token: tokenForUser(userToSave) }); // response to request indicating creation of user.
     }); // save record to database.
 
 
--- models/user.js
+++ models/user.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 // defined model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true }, // unique is case sensitive, so lowercase to remove sensitivity
   password: String
 });
+
+const hashPassword = password => {
+  // generate a salt then run callback
+  const salt = bcrypt.genSaltSync(10); 
+  // hash (encrypt) our password using the salt
+  const hash = bcrypt.hashSync(password, salt, null);
+  return hash;
+  };
+
+
+// On Save Hook, encrypt password
+userSchema.pre("save", function(next) { // "before this model is saved, run function"
+  const user = this; // access current user model
+  const hashedPass = hashPassword(user.password);
+  user.password = hashedPass;
+  next(); // save model now
+})
 
 const ModelClass = mongoose.model("user", userSchema);
 
