7c1d33fa75ce830640fb54b61c96655abf4d3f68
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-03-04 23:56:10 -0800

    Refactored mapsController and added reverse geocoding support

--- controllers/mapsController.js
+++ controllers/mapsController.js
@@ -1,35 +1,71 @@
-const axios = require("axios");
 const keys = require("../config/keys");
-
-const GOOGLE_ROOT_URL = "https://maps.googleapis.com/maps/api/directions/";
-const MAPQUEST_ROOT_URL = "http://www.mapquestapi.com/traffic/v2/incidents";
-const GOOGLE_PLACES_ROOT_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-const GOOGLE_PLACE_DETAILS_ROOT_URL = "https://maps.googleapis.com/maps/api/place/details/json";
-const GOOGLE_PLACE_PHOTOS_ROOT_URL = "https://maps.googleapis.com/maps/api/place/photo";
+const googleMaps = require("../api/googleMaps");
 
 exports.fetchDirections = async (req, res, next) => {
-  const { altRoutes, avoidFerries, avoidHighways, avoidIndoor, avoidTolls, destination, mode, origin, units } = req.body;
+  const { 
+    altRoutes, 
+    avoidFerries, 
+    avoidHighways, 
+    avoidIndoor, 
+    avoidTolls, 
+    destination, 
+    mode, 
+    origin, 
+    units, 
+    currentLocation, // lat lng pair
+    useCurrentLocation // boolean deciding to use `origin` or currentLocation for search
+  } = req.body;
+  // useCurrentLocation, if true, will prioritize currentLocation over inputted origin.
 
-  let BUILDURL = `${GOOGLE_ROOT_URL}json?origin=${origin.replace(/#(?=\S)/g, '')}&destination=${destination.replace(/#(?=\S)/g, '')}
-    &mode=${mode}&alternatives=${altRoutes}&units=${units}`;
+  let avoidArr = [];
+  if (avoidTolls) avoidArr.push("tolls"); 
+  if (avoidHighways) avoidArr.push("highways");
+  if (avoidFerries) avoidArr.push("ferries");
+  if (avoidIndoor) avoidArr.push("indoor");
+  const avoidStr = avoidArr.join("|");
 
-    let avoidArr = [];
-    if (avoidTolls) avoidArr.push("tolls"); 
-    if (avoidHighways) avoidArr.push("highways");
-    if (avoidFerries) avoidArr.push("ferries");
-    if (avoidIndoor) avoidArr.push("indoor");
-    const avoidStr = avoidArr.join("|");
+  try {
 
-    if (avoidStr !== "") BUILDURL += `&avoid=${avoidStr}`; 
-
-    try {
-      const response = await axios.get(BUILDURL + `&key=${keys.googleKey}`);
-      res.send({ directions: response.data, refreshedToken: req.auth });
-    } catch(e) {
-      console.log("Error in fetchDirections:", e);
-      res.status(400).send({ error: "Lookup failed." });
-      next();
+    if (altRoutes === undefined || !destination || !mode || (!origin && !useCurrentLocation) || !units || (useCurrentLocation && !currentLocation)) {
+      res.status(400).send({ error: "Missing required attributes for search. Try to refresh." });
+      return;
     }
+
+    let newOrigin;
+    if (useCurrentLocation) {
+      console.log("Using current location");
+      const revGeoRes = await googleMaps.get(`/geocode/json?latlng=${currentLocation.lat},${currentLocation.lng}&key=${keys.googleKey}`);
+      newOrigin = revGeoRes.data.results[0].formatted_address;
+    } else {
+      newOrigin = origin;
+    }
+
+    const mapsParams = {
+      params: {
+        origin: newOrigin.replace(/#(?=\S)/g, ''),
+        destination: destination.replace(/#(?=\S)/g, ''),
+        mode,
+        alternatives: altRoutes,
+        units,
+        ...avoidStr && { avoid: avoidStr },
+        key: keys.googleKey
+      }
+    }
+
+    const response = await googleMaps.get("/directions/json", mapsParams);
+
+
+
+    res.send({ directions: { ...response.data, origin: newOrigin, destination }, refreshedToken: req.auth });
+
+    return;
+
+  } catch(e) {
+    console.log("Error in fetchDirections:", e);
+    res.status(400).send({ error: "Lookup failed." });
+    next();
+    return;
+  }
     
 
     
