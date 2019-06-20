6401fabd6b1d6ebb679adbbb98974e50d08db67f
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-06-19 22:30:10 -0700

    placesController token refresh refactor and removed old code. Untested, but should be working fine.

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -10,7 +10,6 @@ exports.fetchPlacesWithToken = async (req, res, next) => {
   const { nextPageToken } = res.locals.body;
 
   try {
-    // const placesResponse = await googleMaps.get(`/place/nearbysearch/json`, placesParams);
     const placesResponse = await mapwrap.additionalPlaces(nextPageToken);
 
     return res.send({ 
@@ -18,14 +17,13 @@ exports.fetchPlacesWithToken = async (req, res, next) => {
         results: placesResponse.getResults(), 
         nextPageToken: placesResponse.getNextPageToken() 
       }, 
-      refreshedToken: req.auth
+      refreshedToken: res.locals.auth
     });
   } catch(err) {
     return next(err);
   }
 
 }
-
 
 exports.fetchPlaces = async (req, res, next) => {
   logger.info("Fetching nearby places");
@@ -41,20 +39,10 @@ exports.fetchPlaces = async (req, res, next) => {
     currentLocation, // currentLocation and address may or may not be present. However, at least one will ALWAYS be present.
   } = res.locals.body;
 
-  
-
-  // if (!address && !req.body.currentLocation) {
-  //   res.status(400).send({ error: "Missing required attributes for search. Try to refresh." });
-  //   next();
-  //   return;
-  // }
-
-  // let currentLocation = null;
   if (currentLocation) { 
   //   // optional prop in post request. Used if the user wants to use their current location using browser geolocation
   //   // if specified, takes priority over address prop.
     logger.info("Using current location for places");
-  //   currentLocation = bodyCurrentLocation;
   }
 
   try {
@@ -68,7 +56,6 @@ exports.fetchPlaces = async (req, res, next) => {
     };
 
     if (!currentLocation) {
-      // const geocodedResponse = await googleMaps.get(`/geocode/json?address=${address.replace(/#/g, '')}&key=${keys.googleKey}`);
       const geocodedResponse = await mapwrap.geocode(address.replace(/#/g, ''));
 
       logger.info("Geocoded response:");
@@ -81,16 +68,10 @@ exports.fetchPlaces = async (req, res, next) => {
             center: null,
             address 
           }, 
-          refreshedToken: req.auth
+          refreshedToken: res.locals.auth
         });
       }
 
-      // if (geocodedResponse.data && geocodedResponse.data.results.length === 0) {
-      //   res.send({ places: { results: [], center: null }, refreshedToken: req.auth });
-      //   return;
-      // }
-      
-      // const { lat, lng } = geocodedResponse.data.results[0].geometry.location; // most relevant search result will be used
       logger.info("Getting top address");
       placesParams.location = geocodedResponse.getTopAddress();
       logger.info("Getting formatted top address:");
@@ -111,17 +92,13 @@ exports.fetchPlaces = async (req, res, next) => {
           center: geocodedResponse.getTopAddress(),
           address: formattedAddress
         }, 
-        refreshedToken: req.auth 
+        refreshedToken: res.locals.auth 
       });
     } else {
-      // const { lat, lng } = currentLocation;
       placesParams.location = currentLocation;
 
-      // const placesResponse = await googleMaps.get(`/place/nearbysearch/json`, placesParams);
       const placesResponse = await mapwrap.nearbySearchPlaces(placesParams);
-      // const revGeoRes = await googleMaps.get(`/geocode/json?latlng=${lat},${lng}&key=${keys.googleKey}`);
       const revGeoRes = await mapwrap.reverseGeocode(currentLocation.lat, currentLocation.lng);
-      // const { formatted_address } = revGeoRes.data.results[0];
 
       res.send({ 
         places: { 
@@ -130,13 +107,12 @@ exports.fetchPlaces = async (req, res, next) => {
           center: currentLocation,
           address: revGeoRes.getTopAddress(true)
         }, 
-        refreshedToken: req.auth 
+        refreshedToken: res.locals.auth 
       });
 
       return;
     }
   } catch(err) {
-    // res.status(400).send({ error: "Lookup failed." });
     return next(err);
   }
 }
@@ -145,7 +121,6 @@ exports.fetchPlaceDetails = async (req, res, next) => {
   const { place_id } = res.locals.body;
 
   try {
-    // const response = await googleMaps.get(`/place/details/json?placeid=${place_id}&key=${keys.googleKey}`);
     const placeDetailsResponse = await mapwrap.placeDetails(place_id);
 
     res.send({ 
@@ -153,7 +128,7 @@ exports.fetchPlaceDetails = async (req, res, next) => {
         result: placeDetailsResponse.result, 
         place_id 
       }, 
-      refreshedToken: req.auth 
+      refreshedToken: res.locals.auth 
     });
 
   } catch(err) {
