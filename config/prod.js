d6e5d5058b53bd199ed2b937b1dcde46c4c001d9
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-08-06 23:00:41 -0700

    Adding details to config

--- config/prod.js
+++ config/prod.js
@@ -4,19 +4,19 @@ module.exports = {
   userSecret: process.env.USER_SECRET, // for jwt encoding and decoding
   tokenExpiryTime: process.env.TOKEN_EXPIRY_TIME,
   inactiveTokenTime: process.env.INACTIVE_TOKEN_TIME, // determines how long before an inactive token can no longer be refreshed.
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGO_URI, // now obsolete with the migration of MySQL
   googleKey: process.env.GOOGLE_KEY, // google api key
   googleRecaptchaKey: process.env.GOOGLE_RECAPTCHA_KEY, // google recaptcha secret
   mapquestKey: process.env.MAPQUEST_KEY, // mapquest incidents key
   witaiKey: process.env.WITAI_KEY,
   enableRecaptcha: true, // false if you want to use postman to make testing easier. Should always be true in prod env
   database: {
-    driver: process.env.DB_DRIVER,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    name: process.env.DB_NAME,
-    minConnections: process.env.DB_MIN_CONN,
+    driver: process.env.DB_DRIVER, // name of the underlying data store (MySQL, PostgresQL, MariaDB, etc)
+    host: process.env.DB_HOST, // database host (only the ip)
+    user: process.env.DB_USER, // database login username
+    pass: process.env.DB_PASS, // database login password
+    name: process.env.DB_NAME, // database name (wayfarerdev, wayfarerprod)
+    minConnections: process.env.DB_MIN_CONN, // minimum and max connections in the connection pool
     maxConnections: process.env.DB_MAX_CONN,
   },
 }
